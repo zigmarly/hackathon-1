cf95c886861ed822689b547be915b432264e2e78
Jace <jacemcgough1@gmail.com>
2018-06-02 11:45:24 -0600

    Name in authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
