72dfccfb0eef94d0e0c60296e075832064b43ebd
Jace <jacemcgough1@gmail.com>
2018-06-02 10:38:00 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-  get 'comments/show'
-  get 'comments/new'
-  get 'comments/create'
-  get 'comments/edit'
-  get 'comments/update'
-  get 'comments/destroy'
-  get 'movies/index'
-  get 'movies/show'
-  get 'movies/new'
-  get 'movies/create'
-  get 'movies/edit'
-  get 'movies/update'
-  get 'movies/destroy'
+  resources :movies
   devise_for :users
+
+  root to: "movies#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
