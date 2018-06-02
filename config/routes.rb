f0c330dcc994084370f9b5cc904daedf7efe0155
Jace <jacemcgough1@gmail.com>
2018-06-02 10:50:10 -0600

    Good to go

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :movies
-  devise_for :users
-
-  root to: "movies#index"
-=======
   root to: "movies#index"
   devise_for :users
   
   resources :movies
->>>>>>> The sauce to begin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
