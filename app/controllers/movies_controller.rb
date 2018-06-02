38ebe97aeb5ebf3308a017bbe07dd8674639ca9d
Jace <jacemcgough1@gmail.com>
2018-06-02 11:37:09 -0600

    CRUD for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,47 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update]
+
   def index
+    @movies = current_user.movies
   end
 
   def show
   end
 
   def new
-  end
-
-  def create
+    @movie = Movie.new
   end
 
   def edit
   end
 
-  def update
+  def create
+    @movie = current_user.movies.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def udpate
+    if @movie.update(movie_params)
+      redirect_to movies_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @movie.destroy
   end
+
+  private 
+    def set_movie
+      @movie = current_user.movies.find(params[:id])
+    end
+
+    def movie_params
+      params.require(:movie).permit(:name, :balance)
+    end
 end
