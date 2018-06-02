2328b555fd109d6eacddc60796a29320693f501a
Jace <jacemcgough1@gmail.com>
2018-06-02 10:49:21 -0600

    Good to go

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+<<<<<<< HEAD
     before_action :authenticate_user!
+=======
+  before_action :authenticate_user!
+>>>>>>> The sauce to begin
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   resources :movies
   devise_for :users
 
   root to: "movies#index"
+=======
+  root to: "movies#index"
+  devise_for :users
+  
+  resources :movies
+>>>>>>> The sauce to begin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
