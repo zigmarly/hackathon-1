c680df2c6bb611297fcf116b774a82e0ec1609e0
Jace <jacemcgough1@gmail.com>
2018-06-02 10:15:40 -0600

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/edit'
+  get 'comments/update'
+  get 'comments/destroy'
   get 'movies/index'
   get 'movies/show'
   get 'movies/new'
